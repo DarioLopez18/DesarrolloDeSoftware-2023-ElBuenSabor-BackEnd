527fd21d7ada984cad227cf40f2aa0db35949c8c
chenaoh <darioangellopez38@gmail.com>
2023-11-13 23:26:41 -0300

    carrito paged

--- src/main/java/com/utn/ElBuenSabor/repositories/CarritoRepository.java
+++ src/main/java/com/utn/ElBuenSabor/repositories/CarritoRepository.java
@@ -15,9 +15,9 @@ public interface CarritoRepository extends BaseRepository<Carrito, Long>{
     List<Carrito> findByNumeroCarritoContaining(Integer numeroCarrito);
     Page<Carrito> findByNumeroCarritoContaining(Integer numeroCarrito, Pageable pageable);
 
-    @Query(value = "SELECT c FROM Carrito c WHERE c.numeroCarrito LIKE %:filtro%")
+    @Query(value = "SELECT c FROM Carrito c WHERE c.numeroCarrito = :filtro")
     List<Carrito> search(@Param("filtro") Integer filtro);
 
-    @Query(value = "SELECT c FROM Carrito c WHERE c.numeroCarrito LIKE %:filtro%")
+    @Query(value = "SELECT c FROM Carrito c WHERE c.numeroCarrito = :filtro")
     Page<Carrito> search(@Param("filtro") Integer filtro, Pageable pageable);
 }
